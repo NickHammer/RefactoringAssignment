22998824f0f823a1fe10d93def34b17b9998778e
Nick Hammerstrom <nickh06@hotmail.com>
2021-01-26 17:17:54 -0500

    Fine tuning

--- src/InsuranceStrategyVeryHigh.java
+++ src/InsuranceStrategyVeryHigh.java
@@ -1,11 +1,21 @@
 public class InsuranceStrategyVeryHigh {
-    private final InsuranceCalculator insuranceCalculator;
 
-    public InsuranceStrategyVeryHigh(InsuranceCalculator insuranceCalculator) {
-        this.insuranceCalculator = insuranceCalculator;
+    public InsuranceStrategyVeryHigh() {
     }
 
-    double calculateInsuranceVeryHigh(double income) {
-        return (income - insuranceCalculator.getAdjustment()) * insuranceCalculator.getWeight() + insuranceCalculator.getConstant();
+    public double calculateInsuranceVeryHigh(double income) {
+        return (income - getAdjustment()) * getWeight() + getConstant();
+    }
+
+    public int getConstant() {
+        return 105600;
+    }
+
+    public double getWeight() {
+        return 0.02;
+    }
+
+    public int getAdjustment() {
+        return 60000;
     }
 }
